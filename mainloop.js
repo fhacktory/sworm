bd5ef18731b810a2a1f52926d57d6ca4b9dca633
Rémy Mathieu <remeh@remeh.fr>
2018-01-21 09:59:53 +0100

    good player action.

--- mainloop.js
+++ mainloop.js
@@ -224,12 +224,12 @@ function simulate(state, action) {
     case "rocket":
       let dx = (action.vector[2] - action.vector[0]) / 30;
       let dy = (action.vector[3] - action.vector[1]) / 30;
-      playerShoot(playerName, dx, dy);
+      playerShoot(action.username, dx, dy);
       break;
     case "move":
       let mdx = (action.vector[2] - action.vector[0]) / 30;
       let mdy = (action.vector[3] - action.vector[1]) / 30;
-      playerJump(playerName, mdx, mdy);
+      playerJump(action.username, mdx, mdy);
       break;
     case "spawn":
       state.players = state.players ? state.players : {};
